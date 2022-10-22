2dbc07aa00bab32eade79b8a77e1e6ddd036855b
yuiyuuuu <yingsonyu@gmail.com>
2022-10-21 22:01:30 -0500

    enhanced animation scrolldiv

--- src/home/Scrolldiv.jsx
+++ src/home/Scrolldiv.jsx
@@ -7,7 +7,7 @@ const Scrolldiv = () => {
   const containerRef = useRef(null);
 
   const [vpWidth, setVpWidth] = useState(0);
-  const [smooth, setSmooth] = useState(true);
+  const [smooth, setSmooth] = useState(false);
 
   useEffect(() => {
     const value = window.innerWidth;
@@ -28,6 +28,7 @@ const Scrolldiv = () => {
   useEffect(() => {
     if (!vpWidth) return;
     const init = (value, notfirst) => {
+      console.log("value", value);
       if (document.hidden) {
         return setTimeout(() => {
           init(value, notfirst);
@@ -35,33 +36,142 @@ const Scrolldiv = () => {
       }
 
       if (value === 0 && notfirst) {
-        setSmooth(true);
+        // setSmooth(true);
         return setTimeout(() => {
           containerRef.current.scrollLeft += vpWidth;
           init(value + 1);
-        }, 5000);
+        }, 7600);
       }
 
       if (value === 3) {
         setTimeout(() => {
-          containerRef.current.scrollLeft += vpWidth;
-          setSmooth(false);
-
-          setTimeout(() => {
-            containerRef.current.scrollLeft += -10000;
-            init(0, true);
-          }, 2000);
-        }, 7000);
+          containerRef.current.scrollLeft += -10000;
+          init(0, true);
+        }, 7600);
       } else {
-        setSmooth(true);
+        // setSmooth(true);
         setTimeout(() => {
           containerRef.current.scrollLeft += vpWidth - 15.5;
           init(value + 1);
-        }, 7000);
+        }, 7600);
       }
     };
     init(0);
   }, [vpWidth]);
+
+  useEffect(() => {
+    const init = (num, bool) => {
+      if (document.hidden) {
+        return setTimeout(() => {
+          init(num, bool);
+        }, 500);
+      }
+      if (num === 0 && bool) {
+        gsap.fromTo(
+          "#scroll-one",
+          { opacity: 0, x: "20%" },
+          { opacity: 1, x: 0 }
+        );
+
+        setTimeout(() => {
+          gsap.fromTo(
+            "#scroll-one",
+            { opacity: 1, x: 0 },
+            { opacity: 0, x: "-50%" }
+          );
+
+          setTimeout(() => {
+            init(1);
+          }, 1400);
+        }, 6200);
+
+        return;
+      }
+
+      switch (num) {
+        case 0:
+          gsap.fromTo(
+            "#scroll-one",
+            { opacity: 0, x: "20%" },
+            { opacity: 1, x: 0 }
+          );
+
+          setTimeout(() => {
+            gsap.fromTo(
+              "#scroll-one",
+              { opacity: 1, x: 0 },
+              { opacity: 0, x: "-50%" }
+            );
+
+            setTimeout(() => {
+              init(1);
+            }, 1400);
+          }, 6200);
+          break;
+        case 1:
+          gsap.fromTo(
+            "#scroll-two",
+            { opacity: 0, x: "20%" },
+            { opacity: 1, x: 0 }
+          );
+
+          setTimeout(() => {
+            gsap.fromTo(
+              "#scroll-two",
+              { opacity: 1, x: 0 },
+              { opacity: 0, x: "-50%" }
+            );
+
+            setTimeout(() => {
+              init(2);
+            }, 1400);
+          }, 6200);
+          break;
+
+        case 2:
+          gsap.fromTo(
+            "#scroll-three",
+            { opacity: 0, x: "20%" },
+            { opacity: 1, x: 0 }
+          );
+
+          setTimeout(() => {
+            gsap.fromTo(
+              "#scroll-three",
+              { opacity: 1, x: 0 },
+              { opacity: 0, x: "-50%" }
+            );
+
+            setTimeout(() => {
+              init(3);
+            }, 1400);
+          }, 6200);
+          break;
+
+        case 3:
+          gsap.fromTo(
+            "#scroll-four",
+            { opacity: 0, x: "20%" },
+            { opacity: 1, x: 0 }
+          );
+
+          setTimeout(() => {
+            gsap.fromTo(
+              "#scroll-four",
+              { opacity: 1, x: 0 },
+              { opacity: 0, x: "-50%" }
+            );
+
+            setTimeout(() => {
+              init(0, true);
+            }, 1400);
+          }, 6200);
+          break;
+      }
+    };
+
+    init(0);
+  }, []);
 
   return (
     <div className="container-scroll" id="scrollcontainer">
@@ -79,7 +189,7 @@ const Scrolldiv = () => {
           id="b37"
           style={{ width: vpWidth + "px" }}
         >
-          <div className="container-titledesc">
+          <div className="container-titledesc op0" id="scroll-one">
             <div className="scroll-title">The Milk Tea ATM</div>
             <div className="scroll-desc">
               Mycha is a self-service fridge with a variety of bubble tea, fruit
@@ -92,7 +202,7 @@ const Scrolldiv = () => {
           id="uic"
           style={{ width: vpWidth + "px" }}
         >
-          <div className="container-titledesc">
+          <div className="container-titledesc op0" id="scroll-two">
             <div className="scroll-title">
               Made Fresh with the Best Ingredients
             </div>
@@ -107,7 +217,7 @@ const Scrolldiv = () => {
           id="ucmed"
           style={{ width: vpWidth + "px" }}
         >
-          <div className="container-titledesc">
+          <div className="container-titledesc op0" id="scroll-three">
             <div className="scroll-title">Drinks at Your Convenience</div>
             <div className="scroll-desc">
               No more waiting. Grab and go at our machines in under a minute.
@@ -119,7 +229,7 @@ const Scrolldiv = () => {
           id="rush"
           style={{ width: vpWidth + "px" }}
         >
-          <div className="container-titledesc">
+          <div className="container-titledesc op0" id="scroll-four">
             <div className="scroll-title">Locations across Chicagoland</div>
             <div className="scroll-desc">
               We have many locations across Chicago and constantly adding more.
@@ -131,7 +241,7 @@ const Scrolldiv = () => {
           </div>
         </div>
         <div className="child-scroll" style={{ width: vpWidth + "px" }}>
-          <div className="container-titledesc">
+          <div className="container-titledesc op0" id="scroll-five">
             <div className="scroll-title">The Milk Tea ATM</div>
             <div className="scroll-desc">
               Mycha is a self-service fridge with a variety of bubble tea, fruit
