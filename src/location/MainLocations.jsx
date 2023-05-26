c37f3b89dfcb8beaddf08c624fb64bfe0dbd6d93
Yingson Yu <yingsonyu@gmail.com>
2023-05-25 19:29:04 -0500

    maps part 1

--- src/location/MainLocations.jsx
+++ src/location/MainLocations.jsx
@@ -1,6 +1,15 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import {
+  GoogleMap,
+  Marker,
+  useLoadScript,
+  InfoWindow,
+} from "@react-google-maps/api";
+
 import "./location.scss";
+import "./gmap.scss";
 
 import { location } from "./locationsobj";
 
@@ -11,6 +20,31 @@ import $ from "jquery";
 
 const MainLocations = () => {
   const history = useNavigate();
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_MAPS,
+  });
+
+  const [mapRef, setMapRef] = useState("");
+  const [infoWindowOpen, setInfoWindowOpen] = useState(false);
+  const [infoWindowData, setInfoWindowData] = useState(null);
+
+  const [resultCoordiates, setResultCoordinates] = useState([
+    { address: "address1", lat: 18.52043, lng: 73.856743 },
+    { address: "address2", lat: 12.52043, lng: 73.856743 },
+    { address: "address3", lat: 18.52043, lng: 70.856743 },
+  ]);
+
+  const handleMarkerClickOrHover = (id, lat, lng, address) => {
+    mapRef?.panTo({ lat, lng });
+    setInfoWindowData({ id, address });
+    setInfoWindowOpen(true);
+  };
+
+  const handleMarkerClickOrHoverOut = () => {
+    setInfoWindowData({});
+    setInfoWindowOpen(false);
+  };
 
   useEffect(() => {
     gsap.fromTo(
@@ -31,6 +65,13 @@ const MainLocations = () => {
       { opacity: 1, x: 0, duration: 1.4 }
     );
   }, []);
+
+  const onLoad = (map) => {
+    setMapRef(map);
+    const bounds = new google.maps.LatLngBounds();
+    resultCoordiates?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+    map.fitBounds(bounds);
+  };
 
   useEffect(() => {
     window.scrollTo({ top: 0 });
@@ -152,60 +193,108 @@ const MainLocations = () => {
 
       <div className='heightholder-locations' />
 
-      <div
-        style={{
-          width: "80%",
-          backgroundColor: "white",
-          zIndex: 4,
-        }}
-        className='container-locations'
-        id='container-locations'
-      >
-        <div id='intersecting-locations1' />
-        <div id='intersecting-locations2' />
-        <div id='intersecting-locations3' />
-        {location.map((location, i) => (
-          <div
-            className={`container-info ${window.innerWidth > 700 ? "op0" : ""}`}
-            id={location.id}
-            key={location.id}
+      <div className='locations-e'>
+        {isLoaded && (
+          <GoogleMap
+            mapContainerClassName='gmap-container'
+            center={{ lat: 18.52043, lng: 73.856743 }}
+            zoom={10}
+            onLoad={onLoad}
           >
-            <div className='location-imgcontainer'>
-              <img
-                src={location.image}
-                alt='uiceast'
-                className='img-location'
-              />
-            </div>
-            <div className='location-fdsr'>
-              <div className='location-name '>{location.name}</div>
-              <div className='location-add location-desc'>
-                {location.address}
-              </div>
-              <div className='location-hours location-desc'>
-                {location.hours !== "" ? ` ${location.hours}` : ""}
-              </div>
-
-              <div className='location-checkqty'>
-                <img
-                  src='../assets/leafdivider.png'
-                  style={{ height: "45px", width: "45px", userSelect: "none" }}
-                  alt='leafdivider'
-                />
-                <div
-                  className='check-stock location-desc'
-                  onClick={() =>
-                    history(`/locations/check`, {
-                      state: { from: location.id },
-                    })
-                  }
+            {resultCoordiates.length &&
+              resultCoordiates.map((v, i) => (
+                <Marker
+                  position={{ lat: v.lat, lng: v.lng }}
+                  onClick={() => {
+                    handleMarkerClickOrHover(i, v.lat, v.lng, v.address);
+                  }}
+                  onMouseOver={() => {
+                    handleMarkerClickOrHover(i, v.lat, v.lng, v.address);
+                  }}
+                  onMouseOut={() => {
+                    handleMarkerClickOrHoverOut();
+                  }}
                 >
-                  Check Location Stock
+                  {infoWindowOpen && infoWindowData?.id === i && (
+                    <InfoWindow
+                      onCloseClick={() => {
+                        setInfoWindowOpen(false);
+                      }}
+                    >
+                      <h3>{infoWindowData.address}</h3>
+                    </InfoWindow>
+                  )}
+                </Marker>
+              ))}
+          </GoogleMap>
+        )}
+
+        <div
+          onClick={() =>
+            setResultCoordinates([
+              { lat: 11.52043, lng: 50.856743 },
+              { lat: 12.52043, lng: 50.856743 },
+              { lat: 13.52043, lng: 50.856743 },
+            ])
+          }
+        >
+          change
+        </div>
+
+        <div className='container-locations' id='container-locations'>
+          <div id='intersecting-locations1' />
+          <div id='intersecting-locations2' />
+          <div id='intersecting-locations3' />
+
+          {location.map((location, i) => (
+            <div
+              className={`container-info ${
+                window.innerWidth > 700 ? "op0" : ""
+              }`}
+              id={location.id}
+              key={location.id}
+            >
+              <div className='location-imgcontainer'>
+                <img
+                  src={location.image}
+                  alt='uiceast'
+                  className='img-location'
+                />
+              </div>
+              <div className='location-fdsr'>
+                <div className='location-name '>{location.name}</div>
+                <div className='location-add location-desc'>
+                  {location.address}
+                </div>
+                <div className='location-hours location-desc'>
+                  {location.hours !== "" ? ` ${location.hours}` : ""}
+                </div>
+
+                <div className='location-checkqty'>
+                  <img
+                    src='../assets/leafdivider.png'
+                    style={{
+                      height: "45px",
+                      width: "45px",
+                      userSelect: "none",
+                    }}
+                    alt='leafdivider'
+                  />
+                  <div
+                    className='check-stock location-desc'
+                    onClick={() =>
+                      history(`/locations/check`, {
+                        state: { from: location.id },
+                      })
+                    }
+                  >
+                    Check Location Stock
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       {/* <Background /> */}
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8000,
-  },
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 8000,
+    },
+  });
+};
