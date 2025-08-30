6ce569ba1656bea712b09cdb28f6d5836da1b2f4
fobboyandy <fobboyandy@gmail.com>
2025-08-29 23:41:01 -0700

    Update Checker.jsx

--- src/checker/Checker.jsx
+++ src/checker/Checker.jsx
@@ -52,6 +52,11 @@ const Checker = () => {
         url: `https://mycha-stock-endpoint-fobboyand-604254e78019.herokuapp.com/fetch_stock_location/${loc?.fetchName}`
       })
         .then((res) => {
+
+
+          console.log("fetch_url", fetch_url, "res", res)
+
+
           if (
             res === "not found" ||
             !res ||
@@ -74,8 +79,6 @@ const Checker = () => {
           setDrinks(res);
 
           // let fetch_url = `https://mycha-stock-endpoint-fobboyand-604254e78019.herokuapp.com/fetch_stock_location/${loc?.fetchName}`
-
-          // console.log("fetch_url", fetch_url, "res", res)
 
           setSelectedLocation(loc);
 
